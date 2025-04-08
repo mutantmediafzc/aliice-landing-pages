d21f77d928db0a915dd8a37c41c4d8a80dca2944
j0hnr0 <johnrovanvcabucos@gmail.com>
2025-04-08 14:10:39 +0800

    aesthetics-clinic-booking route

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,6 @@ import Script from "next/script";
 
 export const metadata = {
   title: "Aesthetics - Plastic Surgery & Aesthetic Medicine",
-  description: "TODO: PUT DESCRIPTION",
 };
 
 export default function RootLayout({ children }) {
