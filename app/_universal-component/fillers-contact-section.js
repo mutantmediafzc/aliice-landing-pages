44de1c92d9394f24fc130d22902630e851f67356
j0hnr0 <johnrovanvcabucos@gmail.com>
2025-04-09 17:47:09 +0800

    fillers-booking refractored

---
+++ app/_universal-component/fillers-contact-section.js
@@ -0,0 +1,123 @@
+export default function FillersContactSection({
+  imgOne,
+  imgTwo,
+  title,
+  subTitle,
+  paragraphOne,
+  paragraphTwo,
+  paragraphThree,
+  paragraphFour,
+  appointmentText,
+  btnText,
+  callText,
+  greetings,
+}) {
+  return (
+    <section
+      className="cs_about cs_style_1 bg-custom-white
+        max-custom-sm:!px-[26px]
+        max-custom-md:!px-[12px]"
+      id="about"
+    >
+      <div className="cs_height_120 cs_height_lg_80" />
+      <div className="cs_height_120 cs_height_lg_80" />
+      <div
+        className="container
+      max-custom-md:!max-w-none"
+      >
+        <div
+          className="row cs_gap_y_40 
+        max-custom-md:!flex max-custom-md:!flex-col-reverse"
+        >
+          <div className="col-lg-5 flex flex-col items-center">
+            <div
+              className="cs_about_thumb cs_version_4 position-relative wow fadeInLeft
+            max-w-[436px]"
+            >
+              <img
+                className="max-h-[451px] max-w-[436px]"
+                src={imgOne}
+                alt="About Thumb"
+              />
+            </div>
+            <div
+              className="cs_about_thumb cs_version_4 position-relative wow fadeInLeft
+            max-w-[436px]"
+            >
+              <img
+                className="mt-4 max-h-[451px] max-w-[436px]"
+                src={imgTwo}
+                alt="About Thumb"
+              />
+            </div>
+          </div>
+          <div className="col-lg-7 cs_pl_30">
+            <div className="cs_section_heading cs_style_1">
+              <h3
+                className="cs_section_title_up cs_accent_color_v4 cs_semibold cs_fs_24
+              max-custom-lg:!text-[44px]"
+              >
+                {title}
+              </h3>
+              <h2
+                className="cs_section_title cs_semibold cs_fs_45 mb-0
+               max-custom-lg:!text-[26px]"
+              >
+                {subTitle}
+              </h2>
+            </div>
+            <div className="cs_height_17 cs_height_lg_10" />
+            <p className="cs_about_text cs_type_2">{paragraphOne}</p>
+            <p className="cs_about_text cs_type_2 pl-0">{paragraphTwo}</p>
+            <p className="cs_about_text cs_type_2 pl-0">{paragraphThree}</p>
+            <p className="cs_about_text cs_type_2">{paragraphFour}</p>
+            <p
+              style={{ fontWeight: "bold" }}
+              className="cs_about_text cs_type_2"
+            >
+              {appointmentText}
+            </p>
+            <a
+              href="#service"
+              className="links-div-home-page-a uppercase
+            max-custom-sm:!w-full max-custom-sm:!block max-custom-sm:!max-w-none"
+            >
+              {btnText}
+            </a>
+            <p
+              style={{ fontWeight: "bold", marginTop: 20 }}
+              className="cs_about_text cs_type_2"
+            >
+              {callText}
+            </p>
+            <div style={{ maxWidth: 250 }}>
+              <div className="call-us-flex">
+                <p className="cs_about_text cs_type_2 call-us">Geneva</p>
+                <p className="cs_about_text cs_type_2 span-home-underline call-us-big">
+                  <a href="tel:+41227322223">+41 22 732 22 23</a>
+                </p>
+              </div>
+              <div className="call-us-flex">
+                <p className="cs_about_text cs_type_2 call-us">Gstaad</p>
+                <p className="cs_about_text cs_type_2 span-home-underline call-us-big">
+                  <a href="tel:+41791969869">+41 79 196 98 69</a>
+                </p>
+              </div>
+              <div className="call-us-flex">
+                <p className="cs_about_text cs_type_2 call-us">Montreux </p>
+                <p className="cs_about_text cs_type_2 span-home-underline call-us-big">
+                  <a href="tel:+41799089898">+41 79 908 98 98</a>
+                </p>
+              </div>
+            </div>
+            <p className="cs_about_text cs_type_2">{greetings}</p>
+          </div>
+        </div>
+      </div>
+      <div
+        className="cs_height_120 cs_height_lg_80
+      max-custom-sm:!hidden"
+      />
+    </section>
+  );
+}
--- app/fillers-booking/page.js
+++ app/fillers-booking/page.js
@@ -1,19 +1,115 @@
-import ContactSection from "./_components/contact-section";
-import FirstSection from "./_components/first-section";
-import Footer from "./_components/footer";
-import FormSection from "./_components/form-section";
-import Header from "./_components/header";
-import HeroSection from "./_components/hero-section";
+import FillersContactSection from "../_universal-component/fillers-contact-section";
+import FirstSection from "../_universal-component/first-section";
+import EnFooter from "../_universal-component/footers/en-footer";
+import FormSection from "../_universal-component/form-section";
+import Header from "../_universal-component/header";
+import HeroSection from "../_universal-component/hero-section";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <HeroSection />
-      <FirstSection />
-      <FormSection />
-      <ContactSection />
-      <Footer />
+      <HeroSection
+        title="The gold standard of"
+        mainTitle={
+          <>
+            HYALURONIC <br className="max-custom-lg:!block" /> ACID FILLERS
+          </>
+        }
+        subtitle=""
+        location="in Geneva, Montreux and Gstaad"
+        btnText="ONLINE BOOKING"
+        video="/assets/images/fillers-booking-video.mp4"
+      />
+      <FirstSection
+        img="/assets/images/girl-lips.jpg"
+        mainTitle={
+          <>
+            Fillers made by <br />
+            plastic surgeons
+          </>
+        }
+        subTitle="We are happy to welcome you with:"
+        paragraphOne={
+          <>
+            Free consultation with <b>VISIA SCANNER</b>
+          </>
+        }
+        paragraphTwo="Say goodbye to wrinkles and sagging, add youth to your face!"
+        paragraphThree=""
+        paragraphFour=""
+        btnText="ONLINE BOOKING"
+        goToFormDescription={
+          <>
+            Fill out the form and we will call you back to choose <br /> a
+            convenient time
+          </>
+        }
+        goToFormText="GO TO FORM"
+      />
+      <FormSection
+        title="Price list and consultation booking"
+        subTitle={
+          <>
+            {" "}
+            To schedule a FREE CONSULTATION or receive our detailed PRICE LIST
+            by email, <br /> complete the form.
+          </>
+        }
+        firstNameLabel="First Name*"
+        lastNameLabel="Last Name*"
+        emailLabel="Email*"
+        phoneLabel="Phone number*"
+        serviceLabel="I'm interested in the following service:*"
+        locationLabel="My preferred location is:*"
+        selectText="Please Select"
+        haveAlreadyPatientCheckbox={true}
+        questionLabel="If you have any further questions, please feel free to ask them here!"
+        acceptTermsTextOne={
+          <>
+            Aesthetics Clinic Geneva needs the contact information you provide
+            to us to contact you <br /> about our products and services.
+          </>
+        }
+        acceptTermsTextTwo={
+          <>
+            {" "}
+            By clicking on "Confirm", you are agreeing with the terms listed in
+            our{" "}
+            <span>
+              <a href="#">privacy policy</a>
+            </span>
+          </>
+        }
+        btnText="CONFIRM"
+        btnSubmittingText="Submitting..."
+      />
+      <FillersContactSection
+        imgOne="/assets/images/doctor-girl-1.jpg"
+        imgTwo="/assets/images/doctor-girl-2.jpg"
+        title="Top 10 in Switzerland"
+        subTitle={
+          <>
+            . Led by professor of <br /> injections dr. Tenorio
+          </>
+        }
+        paragraphOne="Our team is trained by our Swiss board certified Doctor, Xavier
+              Tenorio. He is an expert in Plastic, Aesthetic & Reconstructive
+              Surgery (FMH) and an international trainer and injections
+              professor."
+        paragraphTwo="You will have the opportunity to experience a VISIA Scanner which
+              will give you insights on the final your treatment plan."
+        paragraphThree="At our clinic, Dr. Yulia Raspertova, an aesthetics doctor and
+              dermatologist, and Dr. Monia Khedir, family doctor,
+              micro-nutritionist and aesthetics doctor, will be conducting the
+              treatments."
+        paragraphFour="They will be happy to assist you and answer all your questions."
+        appointmentText="To schedule an appointment, please click on the link below"
+        btnText="Online Booking"
+        callText="Or call us on the following number:"
+        greetings="We would be happy to welcome you at Aesthetics Clinic!"
+      />
+      <EnFooter />
     </>
   );
 }
